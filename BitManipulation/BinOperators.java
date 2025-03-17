c895b9d436736eb0fb147f465205c78c71f91ae1
Kushagra <74806834+Kushagra888@users.noreply.github.com>
2025-03-17 22:57:46 +0530

    day 1

---
+++ BitManipulation/BinOperators.java
@@ -0,0 +1,24 @@
+package BitManipulation;
+
+public class BinOperators {
+    public static void main(String args[]) {
+        // binary and
+        System.out.println((2&5));
+
+        // binary or
+        System.out.println((2|5));
+
+        // binary xor
+        System.out.println((2^4));
+
+        // binary 1's complement - not operator
+        System.out.println(~5);
+        System.out.println(~0);
+
+        // binary left shift
+        System.out.println((5<<2));
+
+        // binary right shift
+        System.out.println((6>>2));
+    }
+}

571ed4e9c8621eef210a7bce410064a7282bf47e
Kushagra <74806834+Kushagra888@users.noreply.github.com>
2025-04-27 21:16:26 +0530

    day 4

---
+++ Hashing/TreeSets.java
@@ -0,0 +1,17 @@
+package Hashing;
+import java.util.*;
+
+public class TreeSets {
+    public static void main(String[] args) {
+        TreeSet<String> cities = new TreeSet<>();
+
+        cities.add("Mumbai");
+        cities.add("Odhisa");
+        cities.add("Pune");
+        cities.add("Banglore");
+        cities.add("Delhi");
+
+        System.out.println(cities);
+    }
+    
+}
